6e6a794bf54889a028c189c5bfb2f7b2d80b6371
Joey-Resende <givanildo.resende@gmail.com>
2024-01-30 21:59:34 -0300

    Construindo o inicio do editor.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Element, Sandbox, Settings, Theme};
+use iced::widget::{column, container, text, text_editor};
+
+
+fn main() -> iced::Result {
+    Zion::run(Settings::default())
 }
+
+
+struct Zion {
+    content: text_editor::Content, 
+}
+
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+
+impl Sandbox for Zion {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            content: text_editor::Content::with(include_str!("main.rs")), 
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("A best editor")
+    }
+ 
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        
+        let position = {
+            let (line, column) = self.content.cursor_position();
+
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+
+        container(column![input, position]).padding(10).into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
+    }
+} 
