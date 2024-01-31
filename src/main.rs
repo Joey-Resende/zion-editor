42999c8bdde600226902d62d9844eca88c36c8b2
Joey-Resende <givanildo.resende@gmail.com>
2024-01-31 10:55:59 -0300

    Adicionando o path tracking.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use iced::{Application, Command, Element, Length, Settings, Theme};
 use iced::widget::{button, column, container, horizontal_space, row, text, text_editor};
 
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 fn main() -> iced::Result {
@@ -12,16 +12,17 @@ fn main() -> iced::Result {
 
 
 struct Zion {
+    path: Option<PathBuf>,
     content: text_editor::Content,
     error: Option<Error>,
-}
+} 
 
 
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
     Open,
-    FileOpened(Result<Arc<String>, Error>),
+    FileOpened(Result<(PathBuf, Arc<String>), Error>),
 }
 
 
@@ -34,11 +35,14 @@ impl Application for Zion {
     fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
         (
             Self {
+                path: None, 
                 content: text_editor::Content::new(),
                 error: None,
             }, 
-            Command::perform(load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR")
-            )), Message::FileOpened),
+            Command::perform(
+                load_file(default_file()),
+                Message::FileOpened,
+            ),
         )
     }
 
@@ -54,7 +58,8 @@ impl Application for Zion {
                 Command::none()
             }
             Message::Open => Command::perform(pick_file(), Message::FileOpened),
-            Message::FileOpened(Ok(content)) => {
+            Message::FileOpened(Ok((path, content))) => {
+                self.path = Some(path);
                 self.content = text_editor::Content::with(&content);
 
                 Command::none()
@@ -72,13 +77,19 @@ impl Application for Zion {
 
         let input = text_editor(&self.content).on_edit(Message::Edit);
         
+        let file_path = match self.path.as_deref().and_then(Path::to_str) { 
+            Some(path) => text(path).size(14),
+            None => text(""),
+        };
+
         let position = {
             let (line, column) = self.content.cursor_position();
 
             text(format!("{}:{}", line + 1, column + 1))
         };
 
-        let status_bar = row![horizontal_space(Length::Fill), position];
+        let status_bar = row![file_path, horizontal_space(Length::Fill), position];
+        
         container(column![controls, input, status_bar].spacing(10)).padding(10).into()
     }
 
@@ -88,23 +99,30 @@ impl Application for Zion {
 }
 
 
-async fn pick_file() -> Result<Arc<String>, Error> {
+fn default_file() -> PathBuf {
+    PathBuf::from(format!("{}/srs/main.rs", env!("CARGO_MANIFEST_DIR")))
+}
+
+
+async fn pick_file() -> Result<(PathBuf, Arc<String>), Error> {
     let handle = rfd::AsyncFileDialog::new()
         .set_title("Choose a text file...")
         .pick_file()
         .await
         .ok_or(Error::DialogClosed)?;
 
-    load_file(handle.path()).await
+    load_file(handle.path().to_owned()).await
 }
 
 
-async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, Error> {
-    tokio::fs::read_to_string(path)
+async fn load_file(path: PathBuf) -> Result<(PathBuf, Arc<String>), Error> {
+    let contents = tokio::fs::read_to_string(&path)
         .await
         .map(Arc::new)
         .map_err(|error| error.kind())
-        .map_err(Error::IO)
+        .map_err(Error::IO)?;
+
+    Ok((path, contents))
 }
 
 
